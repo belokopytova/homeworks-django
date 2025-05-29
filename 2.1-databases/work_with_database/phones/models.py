6927581aa7424e35295df88810bea6043088295e
Anna Belokopytova <belo.anna.a91@gmail.com>
2025-05-29 23:53:44 +0300

    changed file

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.CharField(max_length=300)
+    release_date = models.CharField(max_length=15)
+    lte_exists = models.BooleanField()
+    slug = models.CharField(max_length=50)
+
+
+
